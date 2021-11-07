be33c11f7e8bbc11fd98fa9da337ceef4ff8e6e3
mstjepan28 <55623249+mstjepan28@users.noreply.github.com>
2021-11-07 15:14:28 +0100

    Limiting position data in when editing elements

--- components/editElement.vue
+++ components/editElement.vue
@@ -32,8 +32,32 @@ export default {
     data(){
         return{
             isMovable: null,
-            positionData: {},
+            positionData: {
+                x: 0,
+                y: 0,
+                width: 0,
+                height: 0,
+            },
 
+            
+        }
+    },
+    computed:{
+        xCordMax(){
+            const pdfTemplate = document.getElementById("pdfTemplate");
+            return pdfTemplate.offsetWidth - this.positionData.width
+        },
+        yCordMax(){
+            const pdfTemplate = document.getElementById("pdfTemplate");
+            return pdfTemplate.offsetHeight - this.positionData.height
+        },
+        widthDimMax(){
+            const pdfTemplate = document.getElementById("pdfTemplate");
+            return pdfTemplate.offsetHeight - this.positionData.x
+        },
+        heightDimMax(){
+            const pdfTemplate = document.getElementById("pdfTemplate");
+            return pdfTemplate.offsetHeight - this.positionData.y
         }
     },
     methods:{
@@ -52,18 +76,50 @@ export default {
             toggle[this.isMovable]();
         },
 
+        validatePositionData(){
+            Object.keys(this.positionData).forEach(key => {
+                let value = parseInt(this.positionData[key])
+                
+                value = value < 0? 0: value;
+                
+                this.positionData[key] = value;
+            });
 
+            this.positionData.x = this.positionData.x > this.xCordMax? this.xCordMax: this.positionData.x;
+            this.positionData.y = this.positionData.y > this.yCordMax? this.yCordMax: this.positionData.y;
+            this.positionData.width = this.positionData.width > this.widthDimMax? this.widthDimMax: this.positionData.width;
+            this.positionData.height = this.positionData.height > this.heightDimMax? this.heightDimMax: this.positionData.height;
+        },
+
+        updateElementPosition(){
+            if(!this.element || !this.element.positionData) return;
+
+            
+            
+            this.validatePositionData();
+            const element = this.element.elementRef;
+
+            element.style.width = this.positionData.width + "px";
+            element.style.height = this.positionData.height + "px";
+            element.style.transform = 'translate(' + this.positionData.x + 'px,' + this.positionData.y + 'px)';
+        
+            element.dataset.x = this.positionData.x;
+            element.dataset.y = this.positionData.y;
+        }
     },
     watch:{
         element(){
             this.checkIfMovable();
-            this.positionData = this.element.positionData;
+            if(this.element.type == 'selection') this.positionData = this.element.positionData;
         },
         isMovable(){
             this.toggleMovement()
         },
-        positionData(){
-            Object.keys(this.positionData).forEach(key => this.positionData[key] = parseInt(this.positionData[key]))
+        positionData:{
+            handler(){
+                this.updateElementPosition()
+            },
+            deep: true
         }
     }
 }
